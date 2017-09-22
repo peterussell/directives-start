93bbb3b414b7e23924dca254fc9096d6a4157c92
Pete Russell <pinkbatz@gmail.com>
2017-09-22 15:10:43 -0500

    Adds HostListener to react to mouseover and mouseout events.

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,13 +1,23 @@
-import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+import {
+  Directive,
+  Renderer2,
+  OnInit,
+  ElementRef,
+  HostListener
+} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
-  
+
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
+
+  }
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
     /* Use the Renderer instead of directly accessing the DOM (see
        basic-highlight) -- Renderer handles cases where we may not have a
        DOM being rendered, eg. service workers. */
@@ -17,5 +27,12 @@ export class BetterHighlightDirective implements OnInit {
       'blue'
     );
     // NB: there's a 4th arg to setStyle, which are flags. eg. !important
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.renderer.removeStyle(
+      this.elRef.nativeElement,
+      'background-color'
+    );
   }
 }
