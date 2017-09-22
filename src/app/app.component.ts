2a2aaa73a7d521128b0e471c6f1a6417e873c1c2
Pete Russell <pinkbatz@gmail.com>
2017-09-22 12:06:29 -0500

    Initial commit.

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  numbers: number[] = [1, 2, 3, 4, 5];
+  oddOnly = false;
+}
